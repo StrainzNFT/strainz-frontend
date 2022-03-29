7f55724868ed8350e14e14b564eda597ff58e92a
John Doe <johndoe@example.com>
2022-03-29 15:42:03 +0200

    remove dead links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,20 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AboutComponent } from './pages/about/about.component';
 import { StrainDetailComponent } from './pages/strain-detail/strain-detail.component';
 import { StrainzMarketplaceComponent } from './pages/marketplace/strainz-marketplace/strainz-marketplace.component';
-import { NftMarketplaceComponent } from './pages/marketplace/nft-marketplace/nft-marketplace.component';
 import { StrainzInventoryComponent } from './pages/inventory/strainz-inventory/strainz-inventory.component';
 import { NftInventoryComponent } from './pages/inventory/nft-inventory/nft-inventory.component';
-import {VaultsOverviewComponent} from './pages/vaults/vaults-overview/vaults-overview.component';
+import { VaultsOverviewComponent } from './pages/vaults/vaults-overview/vaults-overview.component';
 import { VaultDetailComponent } from './pages/vaults/vault-detail/vault-detail.component';
 import { AccessoryMarketplaceComponent } from './pages/marketplace/accessory-marketplace/accessory-marketplace.component';
 import { StakeOverviewComponent } from './pages/stake-overview/stake-overview.component';
 import { RoadmapComponent } from './pages/roadmap/roadmap.component';
 import { TokenInfoComponent } from './pages/token-info/token-info.component';
-import { CommunityComponent } from './pages/community/community.component';
-import { FaqComponent } from './pages/faq/faq.component';
 import { OffersOverviewComponent } from './pages/offers-overview/offers-overview.component';
 import { MigrationComponent } from './pages/migration/migration.component';
-import {BuystrainzComponent} from "./pages/buystrainz/buystrainz.component";
+import { BuystrainzComponent } from './pages/buystrainz/buystrainz.component';
 
 const routes: Routes = [
   {
@@ -38,15 +35,6 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
-
-  },
-  {
-    path: 'faq',
-    component: FaqComponent
-  },
-  {
-    path: 'community',
-    component: CommunityComponent,
 
   },
   {
@@ -101,10 +89,6 @@ const routes: Routes = [
       {
         path: 'accessories',
         component: AccessoryMarketplaceComponent
-      },
-      {
-        path: 'other',
-        component: NftMarketplaceComponent
       }
     ]
   },
